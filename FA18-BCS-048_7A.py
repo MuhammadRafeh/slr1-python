fd7bca22131c1b074cee43e081526fd1f542fdcd
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-01-18 15:53:52 +0500

    Extracted the Col & Row Index!

--- FA18-BCS-048_7A.py
+++ FA18-BCS-048_7A.py
@@ -48,55 +48,70 @@ productions = {} # {1: {left: 's', right: ['asd']}}
 for index, production in enumerate(grammer):
     rightSide = production.rstrip().split('->')[1].rstrip().split(' ') # ['chased', 'a']
     leftSide = production.rstrip().split('->')[0].rstrip().split(' ')[0] # string
-    productions[index + 1] = { 'left': leftSide, 'right': rightSide }
+    productions[f"{index+1}"] = { 'left': leftSide, 'right': rightSide }
 
 print('\nProductions: ', productions)
 
 # Now initializing Stack
 stack = ['$', '0']
 
-# while inputLoop < len(input):
+while True:
 
-#     char = input[inputLoop]
-#     index_R = all_states.index(stack[-1])
-#     index_C = table[0].index(char)
-#     accept_state = table[index_R][index_C]
+    if len(startingInput) == 0:
+        print('Parsed Failed!!!')
 
-#     if accept_state == "acc":
-#         print("\n")
-#         print("Parsed Successfully")
-#         break
+    tableRowIndex = 1 # Default Value
+    stackTopElement = stack[-1]
+    for index, row in enumerate(fullTable):
+        if row[0] == stackTopElement:
+            tableRowIndex = index
 
-#     elif accept_state.startswith("s"):
+    tableColIndex = 1 # Default Value
+    for index, word in enumerate(tableHeader):
+        if startingInput[0] == word:
+            tableColIndex = index
 
-#         temp = accept_state[1:]
-#         stack.append(char)
-#         print(f"Current Char == {char}\What pushed in Stack Just: \n{stack}")
-#         stack.append(temp)
-#         print(f"Current Char == {char}\nWhat pushed in Stack Just : \n{stack}")
+    operation = fullTable[tableRowIndex][tableColIndex]
+    
+    break
+    # index_C = table[0].index(char)
+    # accept_state = table[index_R][index_C]
+
+    # if accept_state == "acc":
+    #     print("\n")
+    #     print("Parsed Successfully")
+    #     break
+
+    # elif accept_state.startswith("s"):
+
+    #     temp = accept_state[1:]
+    #     stack.append(char)
+    #     print(f"Current Char == {char}\What pushed in Stack Just: \n{stack}")
+    #     stack.append(temp)
+    #     print(f"Current Char == {char}\nWhat pushed in Stack Just : \n{stack}")
         
 
-#     elif accept_state.startswith("r"):
-#         temp = accept_state[1:]
-#         len_grammar = 0
-#         for grammar in productions.items():
-#             if temp == grammar[0]:
-#                 push_symbol = grammar[1].split("->")[0]
-#                 temp_grammar = grammar[1].split("->")[-1]
-#                 len_grammar = len(temp_grammar)
-#                 break
-#         for i in range(len_grammar * 2):
-#             stack.pop()
-#             print(f"Current Char ==> {char}\Pop Action : \n{stack}")
-#         stack.append(push_symbol)
-#         print(f"Current Char ==> {char}\nStack Action Push : \n{stack}")
-#         temp_index_R = all_states.index(stack[-2])
-#         temp_index_C = table[0].index(stack[-1])
-#         temp_accept_state = table[temp_index_R][temp_index_C]
-#         stack.append(temp_accept_state)
-#         print(f"Charater-> {char}\Push in Stack: \n{stack}")
+    # elif accept_state.startswith("r"):
+    #     temp = accept_state[1:]
+    #     len_grammar = 0
+    #     for grammar in productions.items():
+    #         if temp == grammar[0]:
+    #             push_symbol = grammar[1].split("->")[0]
+    #             temp_grammar = grammar[1].split("->")[-1]
+    #             len_grammar = len(temp_grammar)
+    #             break
+    #     for i in range(len_grammar * 2):
+    #         stack.pop()
+    #         print(f"Current Char ==> {char}\Pop Action : \n{stack}")
+    #     stack.append(push_symbol)
+    #     print(f"Current Char ==> {char}\nStack Action Push : \n{stack}")
+    #     temp_index_R = all_states.index(stack[-2])
+    #     temp_index_C = table[0].index(stack[-1])
+    #     temp_accept_state = table[temp_index_R][temp_index_C]
+    #     stack.append(temp_accept_state)
+    #     print(f"Charater-> {char}\Push in Stack: \n{stack}")
 
-#     elif accept_state == "-":
-#         print("\n!!Parse Failed")
-#         break
-#     inputLoop +=1
+    # elif accept_state == "-":
+    #     print("\n!!Parse Failed")
+    #     break
+    # inputLoop +=1
